18cab36ec2687f9c35b1c3eceb572f3183dcc67c
Janai Kasle <janaikalse1@gmail.com>
2024-11-22 22:56:49 +0530

    Update package files and configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
 
   // For specifying sub-page when deploying to gh pages
   // https://vitejs.dev/guide/static-deploy.html#github-pages
-  base: "/OZP_buffer_stat/"
+  base: "/HongKong_Zoning/"
 });
